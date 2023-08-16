668385f35538ec978d0281f2ed0becbdef7eb3d5
Ron McKinnon <ron.mckinnon@uottawa.ca>
2023-08-16 10:22:24 -0400

    Added comments, use proper file extention, e.g. m4a, mp3 based on the original.
    On branch master
    Changes to be committed:
    	modified:   rtrvpod.py

--- rtrvpod.py
+++ rtrvpod.py
@@ -4,19 +4,31 @@ from xml.dom import minidom
 from datetime import datetime
 import subprocess
 
-xmlsrcfile = "planetjarre.podigee.io/feed/mp3"
-xmldestfile = 'rss.xml'
-subprocess.run(['wget', xmlsrcfile, '-O', xmldestfile])
+xmlsrcfile = "planetjarre.podigee.io/feed/mp3"  # default RSS file if none is given
+xmldestfile = 'rss.xml'                         # target file for RSS XML
+subprocess.run(['wget', xmlsrcfile, '-O', xmldestfile]) # get the RSS XML file
 
-rss_dom = minidom.parse(xmldestfile)
-xml_items = rss_dom.getElementsByTagName('item')
+rss_dom = minidom.parse(xmldestfile)  # Use minidom to read the XML file into memory
+xml_items = rss_dom.getElementsByTagName('item') # In a podcast, all the episode info in under <item> tags
 
 n = len(xml_items)
-for item in xml_items:
+for item in xml_items:  # loop through all <item>s
+    # Grab the episode title, replace any slashes / with U+2215 because OSes don't like it in file names.
     title = item.getElementsByTagName('title')[0].firstChild.nodeValue.replace("/", "∕")
-    pubDate = item.getElementsByTagName('pubDate')[0].firstChild.nodeValue
+    # Grab the publication date, and trim off the timezone.
+    pubDate = item.getElementsByTagName('pubDate')[0].firstChild.nodeValue.rsplit(' ', 1)[0]
+    # And most importatly, grab the audio file.
     audio = item.getElementsByTagName('enclosure')[0].getAttribute("url")
-    dt = datetime.strptime(pubDate, '%a, %d %b %Y %H:%M:%S %z')
-    filename = str(n).zfill(3) + ". " + title+ " - " + dt.strftime("%Y-%m-%d") + ".m4a"
+    # Get the file extension
+    fileExt = audio.rsplit('.', 1)[1][:3]
+    # Read the time into a time object
+    dt = datetime.strptime(pubDate, '%a, %d %b %Y %H:%M:%S')
+    # Create the file name which is
+    #   episode number, followed by
+    #   episode name, followed by
+    #   episode date in YYYY-MM-DD format
+    filename = str(n).zfill(3) + ". " + title+ " - " + dt.strftime("%Y-%m-%d") + "." + fileExt
+    # keep count of the number of the episode, starting at the most recent
     n -= 1
+    # Get the episode, renaming it in the process
     subprocess.run(['wget', audio, '-O', filename])
